abfa8bd12d7ad04b501f9c89c2651ea85c2b23e0
MYSUPERPROJECT <https://gitlab.com/snirajan92/mysuperproject.git>
2020-05-07 21:38:14 -0500

    problem 2

--- src/main/java/com/cybr406/post/configuration/SecurityConfiguration.java
+++ src/main/java/com/cybr406/post/configuration/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.cybr406.post.configuration;
 
+import com.cybr406.post.security.user.AccountAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +16,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     H2SecurityConfigurer h2SecurityConfigurer;
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth, AccountAuthenticationProvider provider) throws Exception{
+        auth.authenticationProvider(provider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,5 +36,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+
 
 }
